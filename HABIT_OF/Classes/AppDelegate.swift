94206143a3bf8afe065fc22eb4843abd36c178f9
Goipiot <goipiot@gmail.com>
2019-02-28 08:16:18 +0300

    initial commit

---
+++ HABIT_OF/Classes/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  HABIT_OF
+//
+//  Created by Anton Danilov on 28/02/2019.
+//  Copyright © 2019 Anton Danilov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
